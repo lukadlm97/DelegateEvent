bfa991f3b863acd4f68f0f5e28d9f78fcee0314c
Luka Radovanovic <lukadlm97@gmail.com>
2019-06-23 19:42:29 +0200

    Added first class and project

---
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate void WorkPreformedHendler(int hours, WorkType workType);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void WorkPreformed1(int hours,WorkType workType)
+        {
+            Console.WriteLine("WorkPreformed1 called!");
+        }
+        static void WorkPreformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPreformed2 called!");
+        }
+    }
+    public enum WorkType
+    {
+        GoToMeeetings,
+        Golf,
+        GenerateReports
+    }
+}
